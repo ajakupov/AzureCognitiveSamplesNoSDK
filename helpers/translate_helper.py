0757cbfd2bba8ac594785b08b3ca304045c757d9
ajakupov <jakupov.iitu@gmail.com>
2022-11-01 21:00:24 +0100

    feat: add translator helper

---
+++ helpers/translate_helper.py
@@ -0,0 +1,32 @@
+import os
+import requests
+import json
+import uuid
+
+def translate_text(text):
+    resource_key = os.environ['TRANSLATOR_TEXT_RESOURCE_KEY']
+    region = os.environ['TRANSLATOR_TEXT_REGION']
+    endpoint = os.environ['TRANSLATOR_TEXT_ENDPOINT']
+
+    path = '/translate'
+    params = {
+        'api-version': '3.0',
+        'from': 'en',
+        'to': 'kk'
+    }
+    constructed_url = endpoint + path
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': resource_key,
+        'Ocp-Apim-Subscription-Region': region,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    body = [{
+        'text': text
+    }]
+    request = requests.post(constructed_url, headers=headers, json=body, params=params)
+    response = request.json()
+
+    return response[0]['translations'][0]['text']
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from helpers.audio_helper import get_voice
+from helpers.translate_helper import translate_text
 
 
 if __name__ == '__main__':
-    get_voice()
+    print(translate_text("Hello, world!"))
